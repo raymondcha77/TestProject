b8d98d738a3e66076548de17412946eb7bb75d18
KyongKwon <kkcha@daum.net>
2023-11-16 14:23:38 +0900

    HelloController 완료

---
+++ src/main/java/com/ezen/biz/HelloController.java
@@ -0,0 +1,11 @@
+package com.ezen.biz;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+	public void hello() {
+		System.out.println("Hello!");
+	}
+}
